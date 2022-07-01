35824d10eab4cd08431ef9e5f4f0403ffa18c400
saba <sabera@alphaworks.tech>
2022-06-30 19:01:43 -0700

    create a listener

--- client/src/app/reducers/board.reducer.ts
+++ client/src/app/reducers/board.reducer.ts
@@ -1,4 +1,6 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { ActionChangeSquare } from './../actions/board.actions';
+import { ActionWinner } from 'src/app/actions/board.actions';
 
 
 export const boardFeatureKey = 'board';
@@ -13,5 +15,16 @@ export const initialState: State = {
 
 export const reducer = createReducer(
   initialState,
+  on(ActionChangeSquare, (state, action) => {
+    const { square, value } = action;
+    const squares = [...state.squares];
+    squares[square] = value;
+    return { ...state, squares };
+  }),
+  on(ActionWinner, (state, action) => {
+    const squares = [...state.squares];
+    squares.fill('-');
+    return { ...state, squares };
+  }));
 
-);
+
